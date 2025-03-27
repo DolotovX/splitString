3727db49975423cf444e956bda153d184d762f41
DolotovX <dolotovsergey11@yandex.ru>
2025-03-27 09:07:15 +0300

    Join - Lesson1

--- Lesson1.java
+++ Lesson1.java
@@ -1,9 +1,10 @@
 public class Lesson1 {
     public static void main(String[] args) {
-        String excellent = "Акции$Облигации$Фьючерсы$";
-        String[] splitString = excellent.split("\\$");
-        System.out.println(splitString[0].equals("Акции"));
-        System.out.println(splitString[1].equals("Облигации"));
-        System.out.println(splitString[2].equals("Акции"));
-        }
+        String[] arrayOfcurrency = new String[] {
+            "USD","EUR","GBP"
+        };
+
+        String currency = String.join(",", arrayOfcurrency);
+        System.out.println("Валюты: " + currency);
+    }
     }
