35ced56ce7d6609ceb42b65c72934bcabe4bd778
DolotovX <dolotovsergey11@yandex.ru>
2025-03-27 08:59:26 +0300

    Split - Lesson2

--- Lesson1.java
+++ Lesson1.java
@@ -1,12 +1,9 @@
 public class Lesson1 {
     public static void main(String[] args) {
         String excellent = "Марина Голубева,Анна Ивановка,Васили Рябов,Екатерина Белых,Иван Засонин,";
-        int lastNameStart = 0;
-        for (int i = 0; i < excellent.length(); i++) {
-            if(excellent.charAt(i) == ',') {
-                System.out.println(excellent.substring(lastNameStart,i) + " — отлично");
-                lastNameStart = i + 1;
-            }
+        String[] splitString = excellent.split(",");
+        for (String student: splitString) {
+            System.out.println(student + " — отлично");
         }
     }
 }
