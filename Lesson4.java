57a59b61aa028f49ebe73b87c2002d6a8d82e829
DolotovX <dolotovsergey11@yandex.ru>
2025-04-03 09:57:52 +0300

    Добавили исключение

--- Lesson4.java
+++ Lesson4.java
@@ -1,11 +1,16 @@
 class IntegerConverter {
     public static int convert(final String input) {
-        return Integer.parseInt(input, 10);
+        try {
+            return Integer.parseInt(input, 10);
+        } catch (Throwable throwable) {
+            return 0; // если поймано исключение - вернется 0
+        }
+
     }
 }
 
 public class Lesson4 {
     public static void main(String[] args) {
-        System.out.println(IntegerConverter.convert("10"));
+        System.out.println(IntegerConverter.convert("abc10"));
     }
 }
